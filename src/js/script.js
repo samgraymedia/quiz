3cc296cb1459deb6f0aa88ef50a8a33f8f81c99d
Samuel Gray <sam.gray22@gmail.com>
2018-04-07 12:52:54 +0100

    checking to see if the answer is right or wrong

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 //a var to track which book a user has selected, on load its the first in the array.
 var obj = 0;
+
 $.ajax({
   type: "GET",
   url: "../data/questions.json",
@@ -14,10 +15,19 @@ $.ajax({
     $.each(data, function(i, questions) {
       $('.question').append(questions.title)
       var answers = $.parseJSON(JSON.stringify(questions.answers))
+
       $.each(answers, function(i, answers) {
-        $('.answer').append(answers.answer)
+        $('.answers').append("<button class='answer' data-id="+ answers.id + ">" + answers.answer + "</button>")
 
   });
+});
+$(".answer").click(function(){
+  console.log($(this).attr('data-id'));
+  if ($(this).attr("data-id") !== "6" ) {
+    console.log("WRONG");
+  } else {
+    console.log("RIGHT");
+  }
 });
 
   },
